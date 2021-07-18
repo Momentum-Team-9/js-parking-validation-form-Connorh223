b4e942b6e4b8f872c778217636c0fa86a8e90271
Connor Hickman <Connorh223@gmail.com>
2021-07-18 14:24:41 -0400

    add script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log('Add validation!');
+
+const nameField = document.getElementById("name-field")
+console.log(nameField)
+
+const carField = document.getElementById("car-field")
+console.log(carField)
+
+const daysField = document.getElementById("days-field")
+console.log(daysField)
+
+const startDateField = document.getElementById("start-date-field")
+console.log(startDateField)
+
+const creditCardField = document.getElementById("credit-card-field")
+console.log(creditCardField)
+
+const cvvField = document.getElementById("cvv-field")
+console.log(cvvField)
+
+const expirationField = document.getElementById("expiration-field")
+console.log(expirationField)
+
+const submitButton = document.getElementById("submit-button")
+console.log(submitButton)
+
